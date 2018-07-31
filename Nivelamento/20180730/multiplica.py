a1d73d7789d8367da2ab268cf2ebd1c83787da42
Conta Aluno - IFRN <ivanilsonjunior@gmail.com>
2018-07-30 21:51:01 -0300

    Exercicio de multiplicacao usando soma

---
+++ Nivelamento/20180730/multiplica.py
@@ -0,0 +1,19 @@
+def soma(n1, n2):
+    return n1+n2
+
+def vezes(n1,n2):
+    retorno = 0
+    for i in range(n2):
+        retorno += n1
+    return retorno
+        
+
+
+
+
+n1 = int(input("Digite um multiplicando: "))
+n2 = int(input("Digite o multiplicador: "))
+print ("a soma é igual a : {}".format(vezes(n1,n2)))
+
+
+#4 x 3 = 4+4+4
